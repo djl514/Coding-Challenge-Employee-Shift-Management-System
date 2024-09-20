a7217fafaf3f90b8b50abd471679ef55998d7923
David Layne <davidlayne@usf.edu>
2024-09-20 13:19:45 -0400

    Create assignShift function / still working on if statements

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -5,3 +5,31 @@ const employees = [
     { name: 'David', shifts: [{ day: 'Monday', hours: 8 }] },
     { name: 'Emily', shifts: [{ day: 'Friday', hours: 8 }] }
 ];
+
+//2. Create a function to Display Employee Shift Details
+function displayEmployeeShifts(employee){
+    return employee;
+}
+
+console.log(displayEmployeeShifts(employees[1]));
+
+//3. Create a Function to assign a new shift
+function assignShift(employeeName, days, numHours){
+    let empName = employees.find(worker => worker.name == employeeName)
+    let shiftArray = empName.shifts;
+    shiftArray.forEach(shift => {
+        if (shift.day === days){
+            return `${empName} is already assigned a shift for ${days}`;
+        } else if (shiftArray.day != days){
+            shiftArray.push(
+                { day: days, hours: numHours}
+            );
+    
+        }
+    });
+
+}
+
+
+console.log(assignShift('John', 'Monday', 3));
+console.log(employees);
